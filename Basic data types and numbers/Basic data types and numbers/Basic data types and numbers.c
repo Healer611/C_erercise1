29cf132e457fcc54da9490f8137e906df26247f0
classmate-Han <2207946194@qq.com>
2023-07-15 23:43:18 +0800

    begin learning

---
+++ Basic data types and numbers/Basic data types and numbers/Basic data types and numbers.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int sum(int a)
+//{
+//    int c = 0;
+//    static int b = 3;		//¾²Ì¬
+//    c += 1;
+//    b += 2;
+//    return (a + b + c);
+//}
+//int main()
+//{
+//    int i;
+//    int a = 2;
+//    for (i = 0; i < 5; i++)
+//    {
+//        printf("%d,", sum(a));
+//    }
+//}
+
+
+//ÅÐ¶ÏÊäÈëÊÇ·ñÊÇ×ÖÄ¸
+//int main()
+//{
+//    char ch;
+//    while (scanf(" %c", &ch) != EOF)
+//    {
+//        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+//            printf("%c is an alphabet.\n", ch);
+//        else
+//            printf("%c is not an alphabet.\n", ch);
+//    }
+//	return 0;
+//}

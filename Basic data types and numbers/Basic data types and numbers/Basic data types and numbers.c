b914949091daa67994d88dbfbf943d2f7c7bf8c2
classmate-Han <2207946194@qq.com>
2023-07-24 16:46:09 +0800

    new exercise

--- Basic data types and numbers/Basic data types and numbers/Basic data types and numbers.c
+++ Basic data types and numbers/Basic data types and numbers/Basic data types and numbers.c
@@ -48,19 +48,19 @@
 //}
 
 //显示数据类型和变量长度
-int main()
-{
-	int na, nb;
-	double dx, dy;
-	printf("sizeof(int) = %u\n", (unsigned)sizeof(int));
-	printf("sizeof(double) = %u\n", (unsigned)sizeof(double));
-	printf("sizeof(na) = %u\n", (unsigned)sizeof(na));
-	printf("sizeof(nb) = %u\n", sizeof(nb));
-	printf("sizeof(na+nb) =%u\n", (unsigned)sizeof(na + nb));//int+int为int
-	printf("sizeof(dx+dy) =%u\n", sizeof(dx + dy));//int+double为double
-	printf("sizeof(na+dy) =%u\n", (unsigned)sizeof(na + dy));//double+double为double
-	return 0;
-}
+//int main()
+//{
+//	int na, nb;
+//	double dx, dy;
+//	printf("sizeof(int) = %u\n", (unsigned)sizeof(int));
+//	printf("sizeof(double) = %u\n", (unsigned)sizeof(double));
+//	printf("sizeof(na) = %u\n", (unsigned)sizeof(na));
+//	printf("sizeof(nb) = %u\n", (unsigned)sizeof(nb));
+//	printf("sizeof(na+nb) =%u\n", (unsigned)sizeof(na + nb));//int+int为int
+//	printf("sizeof(dx+dy) =%u\n", (unsigned)sizeof(dx + dy));//int+double为double
+//	printf("sizeof(na+dy) =%u\n", (unsigned)sizeof(na + dy));//double+double为double
+//	return 0;
+//}
 
 
 //求数组a的元素个数的表达式：sizeof(a）/ sizeof(a[0])
@@ -71,6 +71,57 @@ int main()
 //	printf("数组vi的元素个数= %u\n", (unsigned)(sizeof(vi) / sizeof(vi[0])));
 //	printf("数组vd的元素个数= %u\n", (unsigned)(sizeof(vd) / sizeof(vd[0])));
 //	return 0*/;
+//}
+
+
+/*
+	按位运算
+*/
+
+//#include <stdio.h>
+///*--- 返回整数x中设置的位数 ---*/
+//int count_bits(unsigned x)
+//{
+//	int bits = 0;
+//	while (x) {
+//		if (x & 1U) bits++;
+//		x >>= 1;
+//	}
+//	return bits;
+//}
+//
+///*--- 返回unsigned型的位数 ---*/
+//int int_bits(void)
+//{
+//	return count_bits(~0U);
+//}
+//
+///*--- 返回unsigned型的位的内容 ---*/
+//void print_bits(unsigned x)
+//{
+//	int i;
+//	for (i = int_bits() - 1; i >= 0; i--)
+//		putchar(((x >> i) & 1U) ? '1' : '0');
+//}
+//
+//int main(void)
+//{
+//	unsigned a, b;
+//
+//	printf("请输入两个非负整数。\n");
+//	printf("a : ");   scanf("%u", &a);
+//	printf("b : ");   scanf("%u", &b);
+//
+//	printf("\na     = ");   print_bits(a);
+//	printf("\nb     = ");   print_bits(b);
+//	printf("\na & b = ");   print_bits(a & b);		/* a和b的逻辑与 */
+//	printf("\na | b = ");   print_bits(a | b);		/* a和b的逻辑或 */
+//	printf("\na ^ b = ");   print_bits(a ^ b);		/* a和b的逻辑异或 */
+//	printf("\n~a    = ");   print_bits(~a);			/* a的反码 */
+//	printf("\n~b    = ");   print_bits(~b);			/* b的反码 */
+//	putchar('\n');
+//
+//	return 0;
 //}
 
 
--- function/function/function1.c
+++ function/function/function1.c
@@ -313,30 +313,30 @@
 
 //实现一个函数is_prime，判断一个数是不是素数。
 //利用上面实现的is_prime函数，打印100到200之间的素数。
-#include <math.h>
-#include <stdbool.h>
-bool is_prime(int num) 
-{
-    if (num <= 1) 
-        return false;
-
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    int start = 100;
-    int end = 200;
-    for (int i = start; i <= end; i++) {
-        if (is_prime(i)) {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-    return 0;
-}
+//#include <math.h>
+//#include <stdbool.h>
+//bool is_prime(int num) 
+//{
+//    if (num <= 1) 
+//        return false;
+//
+//    for (int i = 2; i * i <= num; i++) {
+//        if (num % i == 0) {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
+//
+//int main() {
+//    int start = 100;
+//    int end = 200;
+//    for (int i = start; i <= end; i++) {
+//        if (is_prime(i)) {
+//            printf("%d ", i);
+//        }
+//    }
+//    printf("\n");
+//    return 0;
+//}
 

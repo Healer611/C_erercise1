247a980ee60c2847e672deaa5f032fe9c4d655ca
classmate-Han <2207946194@qq.com>
2023-09-03 17:19:41 +0800

    查找数组中消失的所有数字

--- C_exerecise/C_exerecise/text2.c
+++ C_exerecise/C_exerecise/text2.c
@@ -17,17 +17,17 @@
 //1 <= n <= 105
 //1 <= nums[i] <= n
 
-#include <stdio.h>
-int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
-    for (int i = 0; i < numsSize; i++) {
-        int x = (nums[i] - 1) % numsSize;
-        nums[x] += numsSize;
-    }
-    int* ret = malloc(sizeof(int) * numsSize);
-    *returnSize = 0;
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] <= numsSize)
-            ret[(*returnSize)++] = i + 1;
-    }
-    return ret;
-}
+//#include <stdio.h>
+//int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+//    for (int i = 0; i < numsSize; i++) {
+//        int x = (nums[i] - 1) % numsSize;
+//        nums[x] += numsSize;
+//    }
+//    int* ret = malloc(sizeof(int) * numsSize);
+//    *returnSize = 0;
+//    for (int i = 0; i < numsSize; i++) {
+//        if (nums[i] <= numsSize)
+//            ret[(*returnSize)++] = i + 1;
+//    }
+//    return ret;
+//}

3abf6e3f427a9531f7140d153ab36ae44bc4b304
classmate-Han <2207946194@qq.com>
2023-09-03 17:21:17 +0800

    寻找峰值

--- C_exerecise/C_exerecise/text5.c
+++ C_exerecise/C_exerecise/text5.c
@@ -1,15 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //Ѱ�ҷ�ֵ
 //https://www.nowcoder.com/share/jump/1181516901693455621949
-int findPeakElement(int* nums, int numsLen) {
-    int left = 0;
-    int right = numsLen - 1;
-    while (left < right) {
-        int mid = ((right - left) >> 1) + left;
-        if (nums[mid] < nums[mid + 1])
-            left = mid + 1;
-        else
-            right = mid;
-    }
-    return left;
-}
+//int findPeakElement(int* nums, int numsLen) {
+//    int left = 0;
+//    int right = numsLen - 1;
+//    while (left < right) {
+//        int mid = ((right - left) >> 1) + left;
+//        if (nums[mid] < nums[mid + 1])
+//            left = mid + 1;
+//        else
+//            right = mid;
+//    }
+//    return left;
+//}

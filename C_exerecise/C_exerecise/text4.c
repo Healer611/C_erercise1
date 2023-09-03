a05061ad8da9962763aedf91deda876740c9754e
classmate-Han <2207946194@qq.com>
2023-09-03 17:20:14 +0800

    寻找出现奇数次的数

--- C_exerecise/C_exerecise/text4.c
+++ C_exerecise/C_exerecise/text4.c
@@ -2,16 +2,16 @@
 
 //寻找出现奇数次的数
 //https://www.nowcoder.com/share/jump/1181516901693639380830
-#include <stdio.h>
-
-int main() {
-    int n, a = 0;
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        int temp = 0;
-        scanf("%d", &temp);
-        a ^= temp;
-    }
-    printf("%d\n", a);
-    return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//    int n, a = 0;
+//    scanf("%d", &n);
+//    for (int i = 1; i <= n; i++) {
+//        int temp = 0;
+//        scanf("%d", &temp);
+//        a ^= temp;
+//    }
+//    printf("%d\n", a);
+//    return 0;
+//}

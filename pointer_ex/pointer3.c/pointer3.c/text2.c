00eba9a53e31c8067a84b6be32d0e9ae8f58dc0d
classmate-Han <2207946194@qq.com>
2023-09-13 21:41:18 +0800

    判断一个字符串是否为另外一个字符串旋转之后的字符串

--- pointer_ex/pointer3.c/pointer3.c/text2.c
+++ pointer_ex/pointer3.c/pointer3.c/text2.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <string.h>
 
 //写一个函数，判断一个字符串是否为另外一个字符串旋转之后的字符串。
 //例如：给定s1 = AABCD和s2 = BCDAA，返回1   
@@ -9,18 +10,39 @@
 //AABCD右旋一个字符得到DAABC
 
 /*AABCDAABCD*/
-#include <string.h>
-void judge(char* a, char* b) {
-	char x[20] = { 0 };
-	strcpy(x, a);
-	strcat(x, a);
-	while(s)
-	printf("%s\n", x);
-}
-int main()
-{
-	char s1[] = "AABCD";
-	char s2[] = "BCDAA";
-	judge(s1, s2);
-	return 0;
-}
+//使用字符串函数一步到位
+//int find(char* a, char* b)
+//{
+//	char tmp[200] = { 0 };
+//	strcpy(tmp, a);
+//	strcat(tmp, a);
+//	return strstr(tmp, b);
+//}
+
+//int judge(char* a, char* b) {
+//	char x[20] = { 0 };
+//	strcpy(x, a);
+//	strcat(x, a);
+//	char* cp = x;
+//	char* s1 = cp;
+//	char* s2 = b;
+//	while (*cp) {
+//		s1 = cp;
+//		s2 = b;
+//		while (*s1 == *s2) {
+//			s1++;
+//			s2++;
+//		}
+//		if (*s2 == '\0')
+//			return 1;
+//		cp++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	char s1[] = "AABCD";
+//	char s2[] = "BCDAA";
+//	printf("%d\n", judge(s1, s2));
+//	return 0;
+//}

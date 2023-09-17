8ab3017248cbe1e426dc4789d6df2a576d419aa8
classmate-Han <2207946194@qq.com>
2023-09-17 19:08:22 +0800

    数据在内存中储存

--- data in memory/data in memory/text1.c
+++ data in memory/data in memory/text1.c
@@ -2,7 +2,19 @@
 #include <stdio.h>
 #include <limits.h>
 
+#include <stdio.h>
+int main()
+{
+	char a = -1;
 
+	signed char b = -1;
+
+	unsigned char c = -1;
+
+	printf("a=%d,b=%d,c=%d", a, b, c);
+
+	return 0;
+}
 //int main()
 //{
 //	//INT_MAX;

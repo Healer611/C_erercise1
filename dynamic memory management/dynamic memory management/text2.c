fa90b297a6ee71cd542ec9732b501e2d01aec778
classmate-Han <2207946194@qq.com>
2023-09-22 23:58:49 +0800

    动态内存管理

---
+++ dynamic memory management/dynamic memory management/text2.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+//void GetMemory(char** p)
+//{
+//	*p = (char*)malloc(100);
+//}
+//
+//int main()
+//{
+//	char* str = NULL;
+//	GetMemory(&str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//	free(str);
+//	str = NULL;
+//	return 0;
+//}

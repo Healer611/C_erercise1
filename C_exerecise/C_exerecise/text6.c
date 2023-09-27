01cb6c5de48f842b79252074fac7f41bd0e6b9c4
classmate-Han <2207946194@qq.com>
2023-09-27 21:34:21 +0800

    珠玑妙算

--- C_exerecise/C_exerecise/text6.c
+++ C_exerecise/C_exerecise/text6.c
@@ -1,9 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //https://leetcode.cn/problems/master-mind-lcci/
 //ÖéçáÃîËã
-int main()
-{
-
-	//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
-	return 0;
-}
+//int main()
+//{
+//
+//	//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
+//	return 0;
+//}

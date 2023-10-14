9e33af52fbe30d1b2ffdeec8a199c8bdc7d9bf6b
classmate-Han <2207946194@qq.com>
2023-10-14 09:20:40 +0800

    练习

--- compile&link/compile&link/exercise.c
+++ compile&link/compile&link/exercise.c
@@ -34,11 +34,74 @@
 
 
 #define MAX(a,b) ((a)>(b)?(a):(b))
-int main()
-{
-	int a = 5;
-	int b = 6;
-	int c = MAX(a++, b++);
-	printf("a=%d\nb=%d\nc=%d\n", a, b, c);
-	return 0;
-}
+
+//int Max(int x, int y)
+//{
+//	return (x > y ? x : y);
+//}
+//int main()
+//{
+//	int a = 5;
+//	int b = 6;
+//	int c = MAX(a++, b++);
+//	printf("a=%d\nb=%d\nc=%d\n", a, b, c);
+//	return 0;
+//}
+
+
+//#define Malloc(num,type) (type*)malloc(num*sizeof(type))
+//
+//int main()
+//{
+//	int* p = (int*)malloc(126 * sizeof(int));
+//	int* p = Malloc(126, int);
+//	return 0;
+//}
+
+//#define INT_PTR int*
+//typedef int* int_ptr;
+//INT_PTR a, b;
+//int_ptr c, d;
+
+
+//#define Print(n,format) printf("the value of "#n" is " format "\n",n)
+//int main()
+//{
+//	int a = 20;
+//	printf("the value of a is %d\n", a);
+//	Print(a,"%d");
+//	return 0;
+//}
+
+//#define MAX(x,y) ((x)>(y)?(x):(y))
+//
+//int Max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//int main()
+//{
+//	int c = MAX(3, 5);
+//	printf("%d\n", c);
+//#undef MAX
+//	c = MAX(5, -5);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//#define WIN 1
+//
+//int main()
+//{
+////#if defined(WIN)
+////	printf("windows\n");
+////#endif
+//
+//#ifdef WIN
+//	printf("windows\n");
+//#endif
+//
+//	return 0;
+//}
+
+
